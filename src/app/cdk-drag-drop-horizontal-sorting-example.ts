184f8cae61bcf36a3860f9dd05cdb97f60cd8b95
Krishna Vankadari <krishnarajivvns@gmail.com>
2019-09-26 17:07:23 -0500

    Initial whatever

---
+++ src/app/cdk-drag-drop-horizontal-sorting-example.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+
+/**
+ * @title Drag&Drop horizontal sorting
+ */
+@Component({
+  selector: 'cdk-drag-drop-horizontal-sorting-example',
+  templateUrl: 'cdk-drag-drop-horizontal-sorting-example.html',
+  styleUrls: ['cdk-drag-drop-horizontal-sorting-example.css'],
+})
+export class CdkDragDropHorizontalSortingExample {
+  timePeriods = [
+    { periord: 'A', year: '1100', ruler: '1000000240-10', dynasty: 'P&G Final Cust', pack: 'Pack A' },
+    { periord: 'A', year: '1100', ruler: '1000000240-10', dynasty: 'P&G Final Cust', pack: 'Pack A' },
+    { periord: 'A', year: '1100', ruler: '1000000240-10', dynasty: 'P&G Final Cust', pack: 'Pack A' },
+    { periord: 'A', year: '1100', ruler: '1000000240-10', dynasty: 'P&G Final Cust', pack: 'Pack A' },
+    { periord: 'A', year: '1100', ruler: '1000000240-10', dynasty: 'P&G Final Cust', pack: 'Pack A' },
+    { periord: 'A', year: '1100', ruler: '1000000240-10', dynasty: 'P&G Final Cust', pack: 'Pack A' },
+    { periord: 'Z', year: '1100', ruler: 'NCWGHBUTT', dynasty: 'NC Order', pack: '   ' },
+
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.timePeriods, event.previousIndex, event.currentIndex);
+  }
+}
+
+
+/**  Copyright 2019 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
